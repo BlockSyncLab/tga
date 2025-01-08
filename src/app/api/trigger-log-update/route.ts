135278f19266c126ecc53e97bc839ab4070a6c5c
Zigoto Cripto <36547842+ZigotoCripto@users.noreply.github.com>
2025-01-08 15:58:22 -0300

    Update route.ts

--- src/app/api/trigger-log-update/route.ts
+++ src/app/api/trigger-log-update/route.ts
@@ -5,19 +5,25 @@ export async function POST(req: Request) {
     const { question, answer } = await req.json();
 
     // Disparar o GitHub Actions (ou realizar qualquer outra ação necessária)
-    await fetch('https://api.github.com/repos/SEU_USUARIO/SEU_REPOSITORIO/actions/workflows/update-log.yml/dispatches', {
+    const response = await fetch('https://api.github.com/repos/BlockSyncLab/tga/actions/workflows/update-log.yml/dispatches', {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,  // GitHub Token que você deve criar em seus segredos
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        ref: 'main',  // Branch onde o workflow do GitHub Actions estará rodando
+        ref: 'master',  // Branch onde o workflow do GitHub Actions estará rodando
         inputs: {
           question,
           answer,
         },
-      });
+      })
+    });
+
+    // Verifica se a resposta foi bem-sucedida
+    if (!response.ok) {
+      throw new Error('Erro ao disparar o GitHub Actions');
+    }
 
     return NextResponse.json({ message: 'Log enviado para atualização no GitHub.' });
 
