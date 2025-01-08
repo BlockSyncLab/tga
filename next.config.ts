395446fabb98832ce21fef26db379bd2b392d678
ZigotoCripto <agomes.santos@gmail.com>
2025-01-07 22:00:25 -0300

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/ask',
+        destination: 'https://walrus-app-xeiqk.ondigitalocean.app/ask',
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
+
 
 export default nextConfig;
